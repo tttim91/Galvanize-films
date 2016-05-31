acfe4445c4ba57df2adc98ba679fc655c240804b
Tim Musgrove <tim.musgrove91@gmail.com>
2016-05-31 11:59:29 -0600

    Adding call to API

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-    $.get('http://api.themoviedb.org/3/movie/550?api_key=3cd423caf6414f9a2c9d9e68ff2c5a24').then(function(data) {
+    $.get('http://api.themoviedb.org/3/search/keyword?api_key=3cd423caf6414f9a2c9d9e68ff2c5a24&query=Jackie').then(function(data) {
         console.log(data);
+        $('h1').click(function() {
+            $('body').append('<p>'+data["results"][1]["name"]+'</p>');
+        });
     });
 });
